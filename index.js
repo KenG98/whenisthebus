264a1cebab6ab91ecf57fd54569afef5f41e3d24
Ken Garber <kengarber98@gmail.com>
2017-10-22 14:16:35 -0400

    finalizing release design

--- index.js
+++ index.js
@@ -6,26 +6,9 @@
 var app = new Vue({
   el: '#app',
   data: {
-    stopTimes: {"Loading...": 0},
-    stationNames: {
-      4068466: "710 Albany St",
-      4114006: "Amory St",
-      4068470: "Hotel Commonwealth",
-      4160730: "Silber Way",
-      4149154: "St. Mary's Way",
-      4110206: "Huntington Ave",
-      4160738: "College of Fine Arts",
-      1: "Student Village 2",
-      2: "Blandford St",
-      3: "Danielsen Hall",
-      4: "Myles Standish",
-      5: "Marsh Plaza",
-      6: "Nickerson Field",
-      7: "",
-      8: "",
-      9: ""
-    },
-    service: "Loading..."
+    stopTimes: { 0: 0 }, // stopID -> [minutes, minutes, minutes]
+    stopNames: { 0: "Loading..." }, // stopID -> "stationName"
+    service: "Loading..." 
   }
 })
 
@@ -34,19 +17,23 @@ var app = new Vue({
  * for the Vue app. Access variables with app.stations.whatever
  */
 
-var getTimes = function() {
+function getTimes() {
   // this isn't necessary yet
   var ms = (new Date()).valueOf()
   // the URL to request
   var requestStr = "https://www.bu.edu/bumobile/rpc/bus/livebus.json.php?callback=jQuery112408381368480827249_1506980760004&_=1506980760313" 
   // the callback which is called when the above URL returns something
   var gotData = function(d) {
-    console.log(d)
+    // console.log(d)
     var firstParen = d.indexOf('(')
     var lastParen = d.indexOf(')')
     var realData = d.slice(firstParen + 1, lastParen)
     realData = JSON.parse(realData)
-    app.service = realData.service
+    if (realData.service == null) {
+      app.service = "No BUS service today"
+    } else {
+      app.service = realData.service
+    }
     var stopTimes = {}
     results = realData.ResultSet.Result
     results.forEach((res) => {
@@ -67,9 +54,13 @@ var getTimes = function() {
   }
   // make and send a request
   var testing = true
-  var testResponse = 'jQuery112408381368480827249_1506980760004({"title":"BU Bus Positions","service":"Late Night Saturday","service_id":"lnw5","ResultSet":{"Result":[{"id":"4010503","call_name":"2020","heading":"7","speed":"0","timestamp":"2017-10-22T00:12:48+00Z","route":"weekday","coordinates":"-71.03097,42.19464","lng":"-71.03097","lat":"42.19464","general_heading":0},{"id":"4007520","call_name":"2016","heading":"339","speed":"1","timestamp":"2017-10-21T23:41:18+00Z","route":"weekday","coordinates":"-71.03103,42.19484","lng":"-71.03103","lat":"42.19484","general_heading":0},{"id":"4007504","call_name":"2127","heading":"245","speed":"0","timestamp":"2017-10-22T03:11:23+00Z","route":"weekday","coordinates":"-71.117193758475,42.351378705207","lng":"-71.117193758475","lat":"42.351378705207","general_heading":225,"arrival_estimates":[{"route_id":"4005182","arrival_at":"2017-10-21T23:13:03-04:00","stop_id":"4160714"},{"route_id":"4005182","arrival_at":"2017-10-21T23:19:05-04:00","stop_id":"4114006"},{"route_id":"4005182","arrival_at":"2017-10-21T23:21:55-04:00","stop_id":"4149154"},{"route_id":"4005182","arrival_at":"2017-10-21T23:23:38-04:00","stop_id":"4068466"},{"route_id":"4005182","arrival_at":"2017-10-21T23:26:04-04:00","stop_id":"4068470"},{"route_id":"4005182","arrival_at":"2017-10-21T23:51:59-04:00","stop_id":"4110206"}]}]},"totalResultsAvailable":3,"isMissingResults":0}); '
   if (testing) {
-    gotData(testResponse)
+    // gotData(testResponse)
+    app.stopTimes = {
+      4114006: [3.5, 13, 23],
+      4160714: [1, 10, 18]
+    }
+    app.service = "Testing Data"
   }
   else {
     var xmlHttp = new XMLHttpRequest()
@@ -82,10 +73,36 @@ var getTimes = function() {
   }
 }
 
+function getStops() {
+  // the URL to request
+  var requestStr = "https://www.bu.edu/bumobile/rpc/bus/stops.json.php?service_id=caloop&callback=jQuery112400993204061098556_1508688412845&_=1508688412846"
+  // the callback which is called when the above URL returns something
+  var gotData = function(d) {
+    // console.log(d)
+    var firstParen = d.indexOf('(')
+    var lastParen = d.indexOf(')')
+    var realData = d.slice(firstParen + 1, lastParen)
+    realData = JSON.parse(realData)
+    var newStopNames = {}
+    realData.ResultSet.Result.forEach(stop => {
+      newStopNames[stop.transloc_stop_id] = stop.stop_name
+    })
+    app.stopNames = newStopNames
+  }
+  // make and send a request
+  var xmlHttp = new XMLHttpRequest()
+  xmlHttp.onreadystatechange = function() { 
+  if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+    gotData(xmlHttp.responseText)
+  }
+  xmlHttp.open("GET", requestStr, true)
+  xmlHttp.send(null)
+}
+
 /** 
  * Finally, call the function to fill in the stations and times.
  */
-
+getStops()
 getTimes()
 
 /** 
